8035d435848358202540a759fcdc48022c7c47ec
Rayen <rayenhamadi48@gmail.com>
2025-12-02 14:09:12 +0100

    Training results

--- Main.py
+++ Main.py
@@ -64,55 +64,55 @@ def plot_data(x, y, colors=('green', 'red'), legend = True):
     plt.savefig("data_plot.png", dpi=140)
     plt.show()    
     
-def plot_results(x_test, y_test, y_pred):
+def plot_results(x_test,y_test, y_pred):
+    '''Affiche un resultat'''
+
+    precision = metrics.precision_score(y_test, y_pred)
+    recall    = metrics.recall_score(y_test, y_pred)
+
+    print("Accuracy = {:5.3f}    Recall = {:5.3f}".format(precision, recall))
+
+    x_pred_positives = x_test[ y_pred == 1 ]     # items prédits    positifs
+    x_real_positives = x_test[ y_test == 1 ]     # items réellement positifs
+    x_pred_negatives = x_test[ y_pred == 0 ]     # items prédits    négatifs
+    x_real_negatives = x_test[ y_test == 0 ]     # items réellement négatifs
+
+    
+    fig, axs = plt.subplots(2, 2)
+    fig.subplots_adjust(wspace=.1,hspace=0.2)
+    fig.set_size_inches(14,10)
+    
+    axs[0,0].plot(x_pred_positives[:,0], x_pred_positives[:,1], 'o',color='lightgreen', markersize=10, label="Prédits positifs")
+    axs[0,0].plot(x_real_positives[:,0], x_real_positives[:,1], 'o',color='green',      markersize=4,  label="Réels positifs")
+    axs[0,0].legend()
+    axs[0,0].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    axs[0,0].set_xlabel('$x_1$')
+    axs[0,0].set_ylabel('$x_2$')
+
+
+    axs[0,1].plot(x_pred_negatives[:,0], x_pred_negatives[:,1], 'o',color='lightsalmon', markersize=10, label="Prédits négatifs")
+    axs[0,1].plot(x_real_negatives[:,0], x_real_negatives[:,1], 'o',color='red',        markersize=4,  label="Réels négatifs")
+    axs[0,1].legend()
+    axs[0,1].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    axs[0,1].set_xlabel('$x_1$')
+    axs[0,1].set_ylabel('$x_2$')
+    
+    axs[1,0].plot(x_pred_positives[:,0], x_pred_positives[:,1], 'o',color='lightgreen', markersize=10, label="Prédits positifs")
+    axs[1,0].plot(x_pred_negatives[:,0], x_pred_negatives[:,1], 'o',color='lightsalmon', markersize=10, label="Prédits négatifs")
+    axs[1,0].plot(x_real_positives[:,0], x_real_positives[:,1], 'o',color='green',      markersize=4,  label="Réels positifs")
+    axs[1,0].plot(x_real_negatives[:,0], x_real_negatives[:,1], 'o',color='red',        markersize=4,  label="Réels négatifs")
+    axs[1,0].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    axs[1,0].set_xlabel('$x_1$')
+    axs[1,0].set_ylabel('$x_2$')
+
+    axs[1,1].pie([precision,1-precision], explode=[0,0.1], labels=["","Errors"], 
+                 autopct='%1.1f%%', shadow=False, startangle=70, colors=["lightsteelblue","coral"])
+    axs[1,1].axis('equal')
+    
+    plt.savefig("results_train_plot.png", dpi=140)
+    plt.show()
         
-        Precision = metrics.precision_score(y_test, y_pred)
-        Recall = metrics.recall_score(y_test, y_pred)
         
-        print(f"Precision: {Precision:.4f}")
-        print(f"Recall: {Recall:.4f}")
-        
-        x_pred_pos = x_test[y_pred == 1]
-        x_real_pos = x_test[y_test == 1]
-        x_pred_neg = x_test[y_pred == 0]
-        x_real_neg = x_test[y_test == 0]
-        
-        fig, axs = plt.subplots(2,2)
-        fig.subplots_adjust(wspace=.1, hspace=.2)
-        fig.set_size_inches(14,10)
-        
-        # Predicted Vs Real positive
-        axs[0,0].plot(x_pred_pos[:, 0], x_pred_pos[:, 1], 'o', markersize=10, label='Predicted Positive')
-        axs[0,0].plot(x_real_pos[:, 0], x_real_pos[:, 1], 'x', markersize=4, label='Real Positive')
-        
-        axs[0,0].legend()
-        axs[0,0].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
-        
-        axs[0,0].set_xlabel('$x_1$')
-        axs[0,0].set_ylabel('$x_2$')
-        
-        # Predicted Vs Real negative
-        axs[0,1].plot(x_pred_neg[:, 0], x_pred_neg[:, 1], 'o', markersize=10, label='Predicted Negative')
-        axs[0,1].plot(x_real_neg[:, 0], x_real_neg[:, 1], 'x', markersize=4, label='Real Negative')
-        axs[0,1].legend()
-        axs[0,1].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
-        axs[0,1].set_xlabel('$x_1$')
-        axs[0,1].set_ylabel('$x_2$')
-        
-        # Superposition 
-        axs[1,0].plot(x_pred_pos[:, 0], x_pred_pos[:, 1], 'o', markersize=10, label='Predicted Positive')
-        axs[1,0].plot(x_real_neg[:, 0], x_real_neg[:, 1], 'x', markersize=4, label='Real Negative')
-        axs[1,0].plot(x_real_pos[:, 0], x_real_pos[:, 1], 'x', markersize=4, label='Real Positive')
-        axs[1,0].plot(x_pred_neg[:, 0], x_pred_neg[:, 1], 'o', markersize=10, label='Predicted Negative')
-        axs[1,0].tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
-        axs[1,0].set_xlabel('$x_1$')
-        axs[1,0].set_ylabel('$x_2$')
-        
-        # camembert of results
-        axs[1,1].pie([Precision, 1-Precision], explode= [0, 0.1], labels=['','Errors'], autopct='%1.1f%%', shadow=False, startangle=70)
-        axs[1,1].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        
-        plt.show()
         
         
 # Parameters Config 
@@ -154,4 +154,17 @@ vector_infos("Training Feature X_train", x_train)
 vector_infos("Training Labels y_train", y_train)
 vector_infos("Testing Feature X_test", x_test)
 vector_infos("Testing Labels y_test", y_test)
+
+
+# Training 
+logreg = LogisticRegression(C=1e5, verbose=1, solver='saga')
+
+logreg.fit(x_train, y_train)
+
+# Predection
+y_pred = logreg.predict(x_test)
+
+# Evaluation
+plot_results(x_test, y_test, y_pred)
+
 
